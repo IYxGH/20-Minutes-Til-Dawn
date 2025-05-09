db5889bc2c8129f19e75dfa06db9e22ec164d1fe
ilia <ilia.varzi@gmail.com>
2025-05-09 15:31:13 +0330

    Login Menu v1

--- core/src/main/java/com/untilldown/Controller/LoginMenuController.java
+++ core/src/main/java/com/untilldown/Controller/LoginMenuController.java
@@ -1,11 +1,37 @@
 package com.untilldown.Controller;
 
+import com.untilldown.Main;
+import com.untilldown.Model.App;
+import com.untilldown.Model.GameAssetManager;
+import com.untilldown.Model.User;
 import com.untilldown.View.LoginMenuView;
+import com.untilldown.View.StartMenuView;
 
 public class LoginMenuController{
     private LoginMenuView view;
 
     public void setView(LoginMenuView view) {
         this.view = view;
+    }
+
+    public void login(String username, String password){
+        User user = App.findUser(username);
+        if(user == null){
+            view.showErrorMessage("there is no user with this username");
+            return;
+        }
+
+        if (!password.equals(user.getPassword())){
+            view.showErrorMessage("wrong password");
+            return;
+        }
+
+        App.setCurrentUser(user);
+//        Main.getMain().setScreen();
+    }
+
+    public void back() {
+        Main.getMain().setScreen(new StartMenuView(new StartMenuController(),
+            GameAssetManager.getGameAssetManager().getSkin()));
     }
 }
--- core/src/main/java/com/untilldown/View/LoginMenuView.java
+++ core/src/main/java/com/untilldown/View/LoginMenuView.java
@@ -1,12 +1,24 @@
 package com.untilldown.View;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.untilldown.Controller.LoginMenuController;
 
 public class LoginMenuView implements Screen {
     private Stage stage;
+    private TextField username;
+    private TextField password;
+    private Label errorMessage;
+    private TextButton loginButton;
+    private TextButton forgetPasswordButton;
+    private TextButton backButton;
 
 
     private final Skin skin;
@@ -14,22 +26,82 @@ public class LoginMenuView implements Screen {
 
     public LoginMenuView(LoginMenuController controller, Skin skin) {
         this.controller = controller;
+        controller.setView(this);
         this.skin = skin;
+
+        username = new TextField("", skin);
+        password = new TextField("", skin);
+        errorMessage = new Label("", skin);
+        errorMessage.setVisible(false);
+        loginButton = new TextButton("Login", skin);
+        forgetPasswordButton = new TextButton("Forget Password", skin);
+        backButton = new TextButton("Back", skin);
+
+
+        loginButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                controller.login(username.getText(), password.getText());
+            }
+        });
+
+        forgetPasswordButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+            }
+        });
+
+        backButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                controller.back();
+            }
+        });
+
     }
 
     @Override
     public void show() {
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+        float screenWidth = Gdx.graphics.getWidth();
+        float screenHeight = Gdx.graphics.getHeight();
+
+        float objectWidth = screenWidth * 0.25f;
+
+
+        Table table = new Table();
+        table.setFillParent(true);
+        table.center();
+
+        table.add(username).expandX().width(objectWidth).height(0.07f * screenHeight);
+        table.row().padTop(0.02f * screenHeight);
+        table.add(password).expandX().width(objectWidth).height(0.07f * screenHeight);
+        table.row().padTop(0.02f * screenHeight);
+        table.add(errorMessage).expandX();
+        table.row().padTop(0.03f * screenHeight);
+
+        table.add(loginButton).expandX().width(objectWidth).height(0.1f * screenHeight);
+        table.row().padTop(0.02f * screenHeight);
+        table.add(forgetPasswordButton).expandX().width(objectWidth).height(0.1f * screenHeight);
+        table.row().padTop(0.02f * screenHeight);
+        table.add(backButton).expandX().width(objectWidth).height(0.1f * screenHeight);
+        table.row().padTop(0.02f * screenHeight);
+        stage.addActor(table);
 
     }
 
     @Override
     public void render(float delta) {
-
+        ScreenUtils.clear(0, 0, 0, 1);
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
@@ -50,5 +122,10 @@ public class LoginMenuView implements Screen {
     @Override
     public void dispose() {
 
+    }
+
+    public void showErrorMessage(String message) {
+        errorMessage.setText(message);
+        errorMessage.setVisible(true);
     }
 }
