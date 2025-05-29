fe0886eaa8cf99534628a38d55ce63ef7beba45f
ilia <ilia.varzi@gmail.com>
2025-05-29 18:22:42 +0330

    Heroes complete

--- core/src/main/java/com/untilldown/Model/Enums/Hero.java
+++ core/src/main/java/com/untilldown/Model/Enums/Hero.java
@@ -12,14 +12,16 @@ public enum Hero {
     SHANA("Heros/Shana/portrait.png",
         4,
         4,
-        new ArrayList<>(Arrays.asList("Heros/Shana/idle/Idle_0 #8330.png",
+        new ArrayList<>(Arrays.asList(
+            "Heros/Shana/idle/Idle_0 #8330.png",
             "Heros/Shana/idle/Idle_1.png",
             "Heros/Shana/idle/Idle_2.png",
             "Heros/Shana/idle/Idle_3.png",
             "Heros/Shana/idle/Idle_4.png",
             "Heros/Shana/idle/Idle_5.png"
             )),
-        new ArrayList<>(Arrays.asList("Heros/Shana/walk/Walk_0.png",
+        new ArrayList<>(Arrays.asList(
+            "Heros/Shana/walk/Walk_0.png",
             "Heros/Shana/walk/Walk_1.png",
             "Heros/Shana/walk/Walk_2.png",
             "Heros/Shana/walk/Walk_3.png",
@@ -29,12 +31,83 @@ public enum Hero {
     DIAMOND("Heros/Diamond/Portrait.png",
         7,
         1,
-        new ArrayList<>(Arrays.asList("")) ,
-        new ArrayList<>(Arrays.asList())
+        new ArrayList<>(Arrays.asList(
+            "Heros/Diamond/idle/Idle (1).png",
+            "Heros/Diamond/idle/Idle (2).png",
+            "Heros/Diamond/idle/Idle (3).png",
+            "Heros/Diamond/idle/Idle (4).png",
+            "Heros/Diamond/idle/Idle (5).png",
+            "Heros/Diamond/idle/Idle (6).png"
+        )) ,
+        new ArrayList<>(Arrays.asList(
+            "Heros/Diamond/walk/Walk (1).png",
+            "Heros/Diamond/walk/Walk (2).png",
+            "Heros/Diamond/walk/Walk (3).png",
+            "Heros/Diamond/walk/Walk (4).png",
+            "Heros/Diamond/walk/Walk (5).png",
+            "Heros/Diamond/walk/Walk (6).png",
+            "Heros/Diamond/walk/Walk (7).png",
+            "Heros/Diamond/walk/Walk (8).png"
+        ))
         ),
-//    SCARLET(),
-//    LILITH(),
-//    DASHER(),
+    SCARLET("Heros/Scarlet/Portrait.png",
+        3,
+        5,
+        new ArrayList<>(Arrays.asList(
+            "Heros/Scarlet/idle/Idle (1).png",
+            "Heros/Scarlet/idle/Idle (2).png",
+            "Heros/Scarlet/idle/Idle (3).png",
+            "Heros/Scarlet/idle/Idle (4).png",
+            "Heros/Scarlet/idle/Idle (5).png",
+            "Heros/Scarlet/idle/Idle (6).png"
+        )) ,
+        new ArrayList<>(Arrays.asList(
+            "Heros/Scarlet/run/Run (1).png",
+            "Heros/Scarlet/run/Run (2).png",
+            "Heros/Scarlet/run/Run (3).png",
+            "Heros/Scarlet/run/Run (4).png"
+        ))
+    ),
+    LILITH("Heros/Lilith/Portrait.png",
+        5,
+        3,
+        new ArrayList<>(Arrays.asList(
+            "Heros/Lilith/idle/Idle (1).png",
+            "Heros/Lilith/idle/Idle (2).png",
+            "Heros/Lilith/idle/Idle (3).png",
+            "Heros/Lilith/idle/Idle (4).png",
+            "Heros/Lilith/idle/Idle (5).png",
+            "Heros/Lilith/idle/Idle (6).png"
+        )) ,
+        new ArrayList<>(Arrays.asList(
+            "Heros/Lilith/walk/Walk (1).png",
+            "Heros/Lilith/walk/Walk (2).png",
+            "Heros/Lilith/walk/Walk (3).png",
+            "Heros/Lilith/walk/Walk (4).png",
+            "Heros/Lilith/walk/Walk (5).png",
+            "Heros/Lilith/walk/Walk (6).png",
+            "Heros/Lilith/walk/Walk (7).png",
+            "Heros/Lilith/walk/Walk (8).png"
+        ))
+    ),
+    DASHER("Heros/Diamond/Portrait.png",
+        2,
+        10,
+        new ArrayList<>(Arrays.asList(
+            "Heros/Dasher/idle/Idle (1).png",
+            "Heros/Dasher/idle/Idle (2).png",
+            "Heros/Dasher/idle/Idle (3).png",
+            "Heros/Dasher/idle/Idle (4).png",
+            "Heros/Dasher/idle/Idle (5).png",
+            "Heros/Dasher/idle/Idle (6).png"
+        )) ,
+        new ArrayList<>(Arrays.asList(
+            "Heros/Dasher/run/Run (1).png",
+            "Heros/Dasher/run/Run (2).png",
+            "Heros/Dasher/run/Run (3).png",
+            "Heros/Dasher/run/Run (4).png"
+        ))
+    ),
     ;
 
 
